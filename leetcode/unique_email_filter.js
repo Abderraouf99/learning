b56644fd023945a8994b62471a5c0e2867ddfef3
Abderraouf <abderraoufbenchoubane@gmail.com>
2021-09-27 22:01:51 -0400

    leet code problem

---
+++ leetcode/unique_email_filter.js
@@ -0,0 +1,51 @@
+const inputEmail = [
+  "test.email+alex@leetcode.com",
+  "test.e.mail+bob.cathy@leetcode.com",
+  "testemail+david@lee.tcode.com",
+];
+
+function filterEmails(emailList) {
+  let uniqueEmails = [];
+  for (const email of emailList) {
+    const splittedEmail = email.split("@");
+    let localName = splittedEmail[0];
+    if (localNameHasDots(localName)) {
+      localName = onHasDotsRemoveDots(localName);
+    }
+    if (localNameHasPlusSign(localName)) {
+      localName = onHasPlusSignIgnoreAfterward(localName);
+    }
+    const formattedEmail = localName + "@" + splittedEmail[1];
+    if (!uniqueEmails.includes(formattedEmail)) {
+      uniqueEmails.push(formattedEmail);
+    }
+  }
+  return uniqueEmails.length;
+}
+
+function localNameHasDots(localName) {
+  return localName.indexOf(".") > -1;
+}
+
+function localNameHasPlusSign(localName) {
+  return localName.indexOf("+") > -1;
+}
+
+function onHasDotsRemoveDots(localName) {
+  let newLocalName = "";
+  for (const char of localName) {
+    if (char !== ".") {
+      newLocalName += char;
+    }
+  }
+  return newLocalName;
+  // if you are using a newer version of node or browser this will work
+  // return localName.replaceAll(".", "");
+}
+function onHasPlusSignIgnoreAfterward(localName) {
+  return localName.split("+")[0];
+}
+
+const result = filterEmails(inputEmail);
+const expected = 2;
+console.log("You answer is \t", result === expected);
